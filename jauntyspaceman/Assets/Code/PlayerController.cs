0ff015a79929f26e61780d19fbcebed0472981f2
Caydo <directorjoe@gmail.com>
2015-03-07 01:44:26 -0700

    Player and Input Update
    Added player controller that has jumping and move left/right.
    Move left/right is commented out.
    The player moves with the camera.
    The jump speed and scroll speeds are configured in editor.

---
+++ jauntyspaceman/Assets/Code/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+  public float JumpSpeed;
+  public float MoveSpeed;
+  public bool IsGrounded = true;
+
+  void Update()
+  {
+    if(IsGrounded && Input.GetKey(KeyCode.Space))
+    {
+      gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * JumpSpeed);
+      IsGrounded = false;
+    }
+
+    // Unneeded but just commenting out to maybe use later
+    //// move left
+    //if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+    //{
+    //  transform.Translate(transform.right * -MoveSpeed * Time.deltaTime);
+    //}
+
+    //// move right
+    //if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+    //{
+    //  transform.Translate(transform.right * MoveSpeed * Time.deltaTime);
+    //}
+  }
+
+  void OnCollisionEnter2D(Collision2D coll)
+  {
+    if(coll.gameObject.tag == "GroundObject")
+    {
+      IsGrounded = true;
+    }
+  }
+}
