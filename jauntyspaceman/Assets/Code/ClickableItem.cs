c7671903f403e42afa2b95ab7efbbc876e0fef3e
Caydo <directorjoe@gmail.com>
2015-03-07 15:46:53 -0700

    Fixing goofy weirdness.

--- jauntyspaceman/Assets/Code/ClickableItem.cs
+++ jauntyspaceman/Assets/Code/ClickableItem.cs
@@ -2,5 +2,13 @@ using UnityEngine;
 
 public class ClickableItem : MonoBehaviour
 {
-  
+  void OnMouseUp()
+  {
+    doAction();
+  }
+
+  protected virtual void doAction()
+  {
+    // do a thing on click
+  }
 }
