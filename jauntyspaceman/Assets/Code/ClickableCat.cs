7be68f538cff22a3e01fbf16da34ab46972fc7ec
Caydo <directorjoe@gmail.com>
2015-03-08 13:00:21 -0600

    Clickable cat disables sprite and makes sparklies. Made default state be fall for player.

--- jauntyspaceman/Assets/Code/ClickableCat.cs
+++ jauntyspaceman/Assets/Code/ClickableCat.cs
@@ -4,6 +4,8 @@ public class ClickableCat : ClickableItem
 {
   protected override void doAction()
   {
-    // add a cat to the trail of cats
+    // turn off cat sprite
+    GetComponent<SpriteRenderer>().enabled = false;
+    GetComponent<ParticleSystem>().Play();
   }
 }
