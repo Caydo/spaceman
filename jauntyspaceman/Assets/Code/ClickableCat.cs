17af2af65d32b27a49183ecef558bf7d8473ac98
Caydo <directorjoe@gmail.com>
2015-03-08 18:27:56 -0600

    Cats explode right now.

--- jauntyspaceman/Assets/Code/ClickableCat.cs
+++ jauntyspaceman/Assets/Code/ClickableCat.cs
@@ -2,11 +2,12 @@ using UnityEngine;
 
 public class ClickableCat : Clicky
 {
+  public SpriteRenderer CatSprite;
   public ParticleSystem CatParticles;
   protected override void doAction()
   {
     // turn off cat sprite
     CatParticles.Play();
-    GetComponent<SpriteRenderer>().enabled = false;
+    CatSprite.enabled = false;
   }
 }
