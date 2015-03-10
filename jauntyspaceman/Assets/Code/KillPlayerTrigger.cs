38577141239afa9f61ebeab0c9bc422ad5e911d5
Caydo <directorjoe@gmail.com>
2015-03-10 13:09:50 -0600

    Kill player after ~2-3 seconds of being stuck.

--- jauntyspaceman/Assets/Code/KillPlayerTrigger.cs
+++ jauntyspaceman/Assets/Code/KillPlayerTrigger.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,25 +12,35 @@ namespace Assets.Code
   {
     int time = 0;
     public int TimeToWaitForStuck = 0;
+    int timeToWaitThenKill = 0;
 
     protected override void doTriggerStayAction()
     {
+      timeToWaitThenKill = TimeToWaitForStuck * 100;
+      StartCoroutine(waitThenUptickTime());
+    }
+
+    IEnumerator waitThenUptickTime()
+    {
       // while the player is in our collider, increment time
-      if (onTriggerStayObject != null)
+      if(onTriggerStayObject != null)
       {
+        // wait a frame then uptick
+        yield return null;
         time++;
       }
-
+      
       // assume the player has tried to get out and can't, so kill them
-      if (time >= TimeToWaitForStuck)
+      if (time >= timeToWaitThenKill)
       {
-		var player = onTriggerStayObject.gameObject.GetComponent<PlayerController>();
-		if(!player.animator.GetBool ("Jump")) {
-			time = 0;
-			player.PlayerDead = true;
-	        GameObject.FindGameObjectWithTag("JetFuelMeter").GetComponent<Slider>().value = 0;
-	        onTriggerStayObject.gameObject.GetComponent<PolygonCollider2D>().enabled = false;
-		}
+        var player = onTriggerStayObject.gameObject.GetComponent<PlayerController>();
+        if(!player.animator.GetBool ("Jump"))
+        {
+          time = 0;
+          player.PlayerDead = true;
+          GameObject.FindGameObjectWithTag("JetFuelMeter").GetComponent<Slider>().value = 0;
+          onTriggerStayObject.gameObject.GetComponent<PolygonCollider2D>().enabled = false;
+        }
       }
     }
   }
