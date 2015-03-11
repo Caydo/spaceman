d73848da5826cf0beafffed189fbc7403e4b15e0
Caydo <directorjoe@gmail.com>
2015-03-10 20:11:09 -0600

    Cats exist! They...do stuff...whatever Kevin wants the animator to do that is...I guess?

---
+++ jauntyspaceman/Assets/Code/CatstellationController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+class CatstellationController : MonoBehaviour
+{
+  List<int> selectedCats = new List<int>();
+  int catSelected = 0;
+  Animator animator;
+  public int MaxCats = 8;
+
+  void Start()
+  {
+    animator = GetComponent<Animator>();
+  }
+
+  public void ClickedCat()
+  {
+    if(selectedCats.Count != MaxCats)
+    {
+      StartCoroutine(getCatNumber());
+    }
+  }
+
+  IEnumerator getCatNumber()
+  {
+    catSelected = Random.Range(0, MaxCats);
+
+    while (selectedCats.Contains(catSelected))
+    {
+      catSelected = Random.Range(1, MaxCats);
+      yield return null;
+    }
+
+    selectedCats.Add(catSelected);
+    animator.SetInteger("SelectedCatNumber", catSelected);
+  }
+}
--- jauntyspaceman/Assets/Code/ClickableCat.cs
+++ jauntyspaceman/Assets/Code/ClickableCat.cs
@@ -8,11 +8,14 @@ public class ClickableCat : Clicky
   private bool clicked = false; 
   protected override void doAction()
   {
-    if(!clicked) { 
+    if(!clicked)
+    { 
       // turn off cat sprite
       CatParticles.Play();
-	  CatSprite.enabled = false;
+	    CatSprite.enabled = false;
       clicked = true;
+
+      GameObject.FindGameObjectWithTag("Catstellation").GetComponent<CatstellationController>().ClickedCat();
     }
   }
 }
